433013e48db9e4cb15ab08c6f2da6cf536168d76
Erich Engler <erich.engler@hotmail.com>
2023-01-17 22:35:20 -0600

    finished stretch problems

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -34,7 +34,7 @@ console.log( supplyChanges );
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-for ( i=0; i<7; i++ ) {
+for ( let i=0; i<7; i++ ) {
     let x = supplyChanges[ i ];
     if ( x > 0 ) {
         console.log( 'Added ' + x + ' parts.' );
@@ -50,12 +50,23 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 for ( let n of supplyChanges ) {
-    
+    if ( n > 0 ) {
+        console.log( 'Added ' + n + ' parts.' );
+    } else if ( n === 0 ) {
+        console.log( 'No change' );
+    } else if ( n < 0 ) {
+        console.log( 'Removed ' + Math.abs( n ) + ' parts.' );
+    }
 }
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalNumberOfParts = 0;
+for ( let i=0; i<supplyChanges.length; i++ ) {
+    totalNumberOfParts += supplyChanges[ i ];
+    console.log( totalNumberOfParts );
+}  // felt pretty great when I finally got this one! :D
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -64,3 +75,24 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let totalParts = 572;
+let boxCapacity = 7;
+let filledBoxes = 0;
+while ( boxCapacity > 0 && totalParts >= 7 && totalParts > 0 ) {
+    totalParts -= 7;
+    boxCapacity -= 1;
+    filledBoxes += 1;
+    console.log( totalParts, ' parts left to box up' );
+
+    if ( filledBoxes === 1 ) {
+        console.log( 'We have filled ' + filledBoxes + ' box.');
+    } else {
+        console.log( 'We have filled ' + filledBoxes + ' boxes.');
+    }
+
+    if ( boxCapacity === 0 ) {
+        boxCapacity += 7;
+    }
+} console.log( 'We have filled ' + filledBoxes + ' boxes and ' + totalParts + ' parts are left over.');
+// WOOOOOW actually can't believe I did it. looking forward to more challenging problems like this!
+
